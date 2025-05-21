dcb496d465746e6563a00bff3ead25a93b0371e5
akinpinkman <akinbilgin54@hotmail.com>
2025-05-21 11:36:05 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './containers/header/header.component';
+import { HeroSliderComponent } from './components/hero-slider/hero-slider.component';
+import { DemoComponent } from './containers/demo/demo.component';
+import { SymptomCheckComponent } from './containers/symptom-check/symptom-check.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    HeroSliderComponent,
+    DemoComponent,
+    SymptomCheckComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'copd';
---
+++ src/app/containers/symptom-check/symptom-check.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-symptom-check',
+  templateUrl: './symptom-check.component.html',
+  styleUrls: ['./symptom-check.component.css']
+})
+export class SymptomCheckComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
