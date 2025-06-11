d968b256a75ac5741588b6553310fc54543eb020
akinpinkman <akinbilgin54@hotmail.com>
2025-06-12 00:03:36 +0300

    .

--- src/app/components/compact-contact-us/compact-contact-us.component.ts
+++ src/app/components/compact-contact-us/compact-contact-us.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-compact-contact-us',
   templateUrl: './compact-contact-us.component.html',
-  styleUrls: ['./compact-contact-us.component.css']
+  styleUrls: ['./compact-contact-us.component.css'],
 })
 export class CompactContactUsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
