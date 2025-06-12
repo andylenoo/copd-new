e542f8cc9f8cd0eb9d1c315988c042fd9d1e90f8
akinpinkman <akinbilgin54@hotmail.com>
2025-06-12 21:24:18 +0300

    update

--- src/app/containers/header/header.component.ts
+++ src/app/containers/header/header.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -8,4 +9,14 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./header.component.css'],
   imports: [RouterModule, CommonModule],
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  currentRoute: string = 'false';
+  constructor(private router: Router) {}
+  ngOnInit(): void {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.currentRoute = event.urlAfterRedirects;
+      });
+  }
+}
