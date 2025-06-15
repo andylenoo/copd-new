5687798fb58fe8e831e03bea5387cb62f9f35d7f
akinpinkman <akinbilgin54@hotmail.com>
2025-06-15 12:05:19 +0300

    .

--- src/app/pages/about-copd/about.component.ts
+++ src/app/pages/about-copd/about.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CompactContactUsComponent } from '../../components/compact-contact-us/compact-contact-us.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-about-page',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css'],
-  imports: [CompactContactUsComponent],
+  imports: [CompactContactUsComponent, RouterModule],
 })
 export class AboutPageComponent implements OnInit {
   constructor() {}
